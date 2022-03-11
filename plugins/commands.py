f7faecd5369a2942d6b319b17708c3e5b2c66926
amikra <com.ibbs09@gmai.com>
2022-03-11 17:30:38 +0530

    Initial commit

---
+++ plugins/commands.py
@@ -0,0 +1,138 @@
+#MIT License
+
+#Copyright (c) 2021 subinps
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+from config import Config
+import asyncio
+import sys
+import os
+
+USER=Config.USER
+OWNER=Config.OWNER
+HOME_TEXT=Config.HOME_TEXT
+HOME_TEXT_OWNER=Config.HOME_TEXT_OWNER
+HELP=Config.HELP
+
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd):
+	if str(cmd.from_user.id) != OWNER:	
+		await cmd.reply_text(
+			HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id, USER, USER, USER, OWNER), 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("😁Developer", url='https://t.me/amikra'),
+						InlineKeyboardButton("(¬‿¬)Other Channel", url="https://t.me/mushtaque09")
+					],
+                    [
+                        InlineKeyboardButton("Instagram ID", url="https://instagram.com/am.ikraa"),
+						InlineKeyboardButton("Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/amikra/amikra")
+                    ],
+                    [
+                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#amikra"),
+						InlineKeyboardButton("😃YT Channel", url="https://youtube.com/c/VFXABHI")
+
+                    ]
+					
+				]
+			)
+		)
+	else:
+		await cmd.reply_text(
+			HOME_TEXT_OWNER.format(cmd.from_user.first_name, cmd.from_user.id), 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("😁Developer", url='https://t.me/amikra'),
+						InlineKeyboardButton("(¬‿¬)Other Channel", url="https://t.me/mushtaque09"),
+					],
+                    [
+                        InlineKeyboardButton("Instagram ID", url="https://instagram.com/am.ikraa")
+                    ],
+                    [
+                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#amikra"),
+						InlineKeyboardButton("😃YT Channel", url="https://youtube.com/c/VFXABHI")
+
+                    ]
+					
+				]
+			)
+		)
+
+
+@Client.on_message(filters.command("help") & filters.private)
+async def help(bot, cmd):
+	await cmd.reply_text(
+		HELP,
+		disable_web_page_preview=True,
+		reply_markup=InlineKeyboardMarkup(
+			[
+				[
+					InlineKeyboardButton("😁Developer", url='https://t.me/amikra'),
+					InlineKeyboardButton("(¬‿¬)Other Channel", url="https://t.me/mushtaque09"),
+                    InlineKeyboardButton("😃YT Channel", url="https://youtube.com/c/VFXABHI")
+				],
+				[
+					InlineKeyboardButton("Instagram ID", url="https://instagram.com/am.ikraa"),
+					InlineKeyboardButton("Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/amikra/amikra")
+				]
+			]
+			)
+		)
+
+@Client.on_message(filters.command("restart") & filters.private)
+async def stop(bot, cmd):
+	if str(cmd.from_user.id) != OWNER:	
+		await cmd.reply_text(
+			HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id, USER, USER, USER, OWNER), 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("😁Developer", url='https://t.me/amikra'),
+						InlineKeyboardButton("(¬‿¬)Other Channel", url="https://t.me/mushtaque09")
+					],
+                    [
+                        InlineKeyboardButton("Instagram ID", url="https://instagram.com/am.ikraa"),
+						InlineKeyboardButton("Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/amikra/amikra")
+                    ],
+                    [
+                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#amikra"),
+						InlineKeyboardButton("😃YT Channel", url="https://youtube.com/c/VFXABHI")
+
+                    ]
+					
+				]
+			)
+		)
+		return
+	msg = await bot.send_message(
+		text="Restarting your bot..",
+		chat_id=cmd.from_user.id
+		)
+	await asyncio.sleep(2)
+	await msg.edit("All Processes Stopped and Restarted")
+	os.execl(sys.executable, sys.executable, *sys.argv)
